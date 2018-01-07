b772704e40d844ccfbbea8fd4f5517acbdc79b2b
Vic Patel <vp932712@gmail.com>
2018-01-07 18:33:36 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,12 +1,8 @@
-# def alphabetize(arr)
-#
-#
-#
-# end
+
 
 def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
-  # alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
 
   arr.sort_by do |word|
     word.chars.map { |c| esperanto.index(c) }
