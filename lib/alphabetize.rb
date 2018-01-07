b3ecd656970d554701d8dd5c089f128c97236a75
Vic Patel <vp932712@gmail.com>
2018-01-07 18:31:07 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
-def alphabetize(arr)
-  # code here
+# def alphabetize(arr)
+#   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+#
+#
+# end
+
+def alphabetize(phrases)
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+  phrases.sort_by do |phrase|
+    phrase.chars.map { |c| alpha.index(c) }
+  end
 end
